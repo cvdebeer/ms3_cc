a63136d29a141ce44b1d80297d6f714a6ecce487
cvdebeer <claudiadiebeer@gmail.com>
2020-01-10 12:46:27 +0000

    set up check that  app.py is running correctly

---
+++ app.py
@@ -0,0 +1,14 @@
+import pymongo
+import os
+from flask import Flask
+
+app = Flask(__name__)
+
+
+@app.route('/')
+def hello_world():
+    return 'Hello Big bad World'
+
+
+if __name__ == "__main__":
+    app.run()
