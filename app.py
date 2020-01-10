b6cbba4eec4320026d23cf9c2d32838fcacef037
cvdebeer <claudiadiebeer@gmail.com>
2020-01-10 13:35:16 +0000

    link to MongoDB

--- app.py
+++ app.py
@@ -1,14 +1,20 @@
-import pymongo
 import os
-from flask import Flask
+from flask import Flask, render_template, redirect, request, url_for
+from flask_pymongo import PyMongo
+from bson.objectid import ObjectId
 
 app = Flask(__name__)
+app.config['MONGO_DBNAME'] = 'ccRecipes'
+app.config['MONGO_URI'] = os.getenv('MONGO_URI')
 
+mongo = PyMongo(app)
 
 @app.route('/')
 def hello_world():
-    return 'Hello Big bad World'
+    return 'Hello Big beautiful World'
 
 
 if __name__ == "__main__":
-    app.run()
+    app.run(host=os.environ.get('IP'),
+            port=int(os.environ.get('PORT')),
+            debug=True)
