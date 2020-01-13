0d32581713a05183f0d68113aae725b253884392
cvdebeer <claudiadiebeer@gmail.com>
2020-01-13 10:13:21 +0000

    support upload, can't get value

--- static/js/main.js
+++ static/js/main.js
@@ -1,3 +1,32 @@
+const addIngredient = document.querySelector('#add-ing');
+const addLineIngredient = document.querySelector('#adding-ingredients');
+const plusMethod = document.querySelector('#add-met')
+const addMethod = document.querySelector('add-method')
+const method = document.getElementsByName('method')
+
+let amount = document.getElementsByName('amount').values();
+let measurement = document.getElementsByName('measurement').values();
+let ingredient = document.getElementsByName('ingredient').values();
+let ingredients = [];
+let methods = [];
+
 $(document).ready(function () {
     $('select').formSelect();
+
 });
+
+
+function addIngLine() {
+
+    ingredients.push(amount, measurement, ingredient);
+    console.log(ingredients)
+}
+
+function addingMethod() {
+
+    let inputMethod = document.getElementsByClassName('add-method').value;
+    alert(inputMethod);
+}
+
+addIngredient.addEventListener('click', addIngLine);
+plusMethod.addEventListener('click', addingMethod);
