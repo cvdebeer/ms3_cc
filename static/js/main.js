16750f1a48b01759b759e8a8c27e0f0a598dbb01
cvdebeer <claudiadiebeer@gmail.com>
2020-01-15 20:11:21 +0000

    render lists for methods& ingredients. Show image

--- static/js/main.js
+++ static/js/main.js
@@ -10,6 +10,7 @@ const info = document.querySelector('#recipe-info');
 let ingredients = [];
 let methods = [];
 
+
 $(document).ready(function () {
     $('select').formSelect();
 
@@ -19,20 +20,48 @@ function addIngLine() {
     let amount = document.getElementById('amount').value;
     let measurement = document.getElementById('measurement').value;
     let ingredient = document.getElementById('ingredient').value;
-    ingredients.push(amount, measurement, ingredient);
-    addLineIngredient.insertAdjacentHTML('beforebegin',
-        `<ul><li><i class="material-icons left">donut_small</i>${amount} ${measurement} ${ingredient}</li></ul>`);
+    ingredients.push([amount, measurement, ingredient]);
+    addLineIngredient.insertAdjacentHTML('beforeend',
+        `<li>${amount} ${measurement} ${ingredient}</li>`);
+    console.log(ingredients);
+
 }
 
 function addingMethod() {
     let inputMethod = document.getElementById('method').value;
-    methods.push(inputMethod)
-    addMethod.insertAdjacentHTML('beforebegin',
-        `<ul><li><i class="material-icons left">donut_small</i>${inputMethod}</li></ul>`);
+    methods.push(inputMethod);
+
+    addMethod.insertAdjacentHTML('beforeend',
+        `<li>${inputMethod}</li>`);
+
 }
 
 function imageUpload() {
     pic.click();
+    /*Code for previewing images found https://stackoverflow.com/questions/31710127/javascript-image-upload-and-display */
+    let fileTag = document.getElementById("fileInput"),
+        preview = document.getElementById("preview"),
+        display_image = document.getElementById('display-img');
+
+    fileTag.addEventListener("change", function () {
+        changeImage(this);
+    });
+
+    function changeImage(input) {
+        let reader;
+
+        if (input.files && input.files[0]) {
+            reader = new FileReader();
+
+            reader.onload = function (e) {
+                preview.setAttribute('src', e.target.result);
+                display_image.setAttribute('src', e.target.result)
+            }
+
+            reader.readAsDataURL(input.files[0]);
+        }
+    }
+
 }
 
 function reviewRecipe() {
@@ -40,20 +69,35 @@ function reviewRecipe() {
     let serve = document.getElementById('servings').value;
     let prep = document.getElementById('prep_time').value;
     let cook = document.getElementById('cook_time').value;
+    result = Number(prep) + Number(cook);
     let rate = document.getElementById('rating').value;
     let carb = document.getElementById('carbs').value;
     let fats = document.getElementById('fat').value;
     let pro = document.getElementById('protein').value;
+    /* Getting preview of image */
+
 
     document.getElementById('display-r-n').innerHTML = name;
     document.getElementById('display-serv').innerHTML = serve;
     document.getElementById('display-prep').innerHTML = prep;
     document.getElementById('display-cook').innerHTML = cook;
+    document.getElementById('display-total').innerHTML = result;
     document.getElementById('display-carb').innerHTML = carb;
     document.getElementById('display-fat').innerHTML = fats;
     document.getElementById('display-pro').innerHTML = pro;
     document.getElementById('display-rating').innerHTML = rate;
-    console.log(name);
+
+
+    for (let i = 0; i < ingredients.length; i++) {
+        document.getElementById('display-ing').insertAdjacentHTML("beforeend",
+            `<li>${ingredients[i].join(' ')}</li>`)
+    };
+
+    for (let m = 0; m < methods.length; m++) {
+        document.getElementById('display-method').insertAdjacentHTML('beforeend',
+            `<li>${methods[m]}</li>`)
+    };
+
 }
 
 
